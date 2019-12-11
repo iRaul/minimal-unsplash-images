332bcd4406596a9b42878744d2dfc5584b76ccaa
Raul Dronca <rauldronk@gmail.com>
2019-12-11 16:06:37 +0200

    linting

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,32 +4,32 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import Loader from './Loader';
 
 type Props = {
-	dataLength: number;
-	next: () => void;
+  dataLength: number;
+  next: () => void;
 };
 
 const breakPoints = {
-	default: 3,
-	768: 2,
+  default: 3,
+  768: 2,
 };
 
 const Grid: React.FC<Props> = ({ dataLength, next, children }) => {
-	return (
-		<InfiniteScroll
-			dataLength={dataLength}
-			next={next}
-			hasMore
-			loader={<Loader />}
-		>
-			<Masonry
-				breakpointCols={breakPoints}
-				className='m-grid'
-				columnClassName='m-grid__column'
-			>
-				{children}
-			</Masonry>
-		</InfiniteScroll>
-	);
+  return (
+    <InfiniteScroll
+      dataLength={dataLength}
+      next={next}
+      hasMore
+      loader={<Loader />}
+    >
+      <Masonry
+        breakpointCols={breakPoints}
+        className='m-grid'
+        columnClassName='m-grid__column'
+      >
+        {children}
+      </Masonry>
+    </InfiniteScroll>
+  );
 };
 
 export default Grid;
